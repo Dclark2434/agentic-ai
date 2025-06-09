e7ca0c2567c8e265ca55d6e366bef3198b4469a4
Dustin Clark <138034989+Dclark2434@users.noreply.github.com>
2025-06-09 13:47:10 -0500

    Refactor main function to handle command-line prompts and improve content generation flow

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 from google import genai
 import os
 from dotenv import load_dotenv
+import sys
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
 
-response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents='Where can I find spice on Arrakis?'
-)
-prompt_token_count = response.usage_metadata.prompt_token_count
-prompt_response_count = response.usage_metadata.candidates_token_count
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001', contents=prompt
+    )
+    prompt_token_count = response.usage_metadata.prompt_token_count
+    prompt_response_count = response.usage_metadata.candidates_token_count
 
-print(response.text)
-print(f"Prompt tokens: {prompt_token_count}")
-print(f"Response tokens: {prompt_response_count}")
+    print(response.text)
+    print(f"Prompt tokens: {prompt_token_count}")
+    print(f"Response tokens: {prompt_response_count}")
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        prompt = sys.argv[1]
+        main()
+    else:
+        sys.exit("Please provide a prompt. Example: python3 main.py \"where all the spice melange at?\"")
