27b1b3dffa430298fc06d3a365e38d0528ed3f86
Dustin Clark <138034989+Dclark2434@users.noreply.github.com>
2025-06-11 10:27:13 -0500

    Enhance main function with conversation tracking and verbose output options

--- main.py
+++ main.py
@@ -10,15 +10,17 @@ def main(prompt):
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
+    # adds turns to list so the model can review the conversation with each prompt
     messages = [
         types.Content(role="user", parts=[types.Part(text=prompt)]),
     ]
 
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001',
+        model='gemini-2.0-flash-001', # cheaper free tier model
         contents=messages
     )
 
+    # creates verbose output if called
     prompt_token_count = response.usage_metadata.prompt_token_count
     prompt_response_count = response.usage_metadata.candidates_token_count
     if len(sys.argv) > 2:
@@ -27,7 +29,8 @@ def main(prompt):
             print(f"User prompt: {prompt}")
             print(f"Prompt tokens: {prompt_token_count}")
             print(f"Response tokens: {prompt_response_count}")
-
+    
+    # prints response to console
     print(response.text)
 
 
