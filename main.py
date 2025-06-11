f2405aae5bb0165f29eae385f8b7f9e0ea36e120
Dustin Clark <138034989+Dclark2434@users.noreply.github.com>
2025-06-11 10:24:16 -0500

    Refactor main function to accept prompt as an argument and created verbose output flag

--- main.py
+++ main.py
@@ -1,26 +1,39 @@
 from google import genai
-import os
+from google.genai import types
 from dotenv import load_dotenv
+import os
 import sys
 
-def main():
+def main(prompt):
+
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=prompt)]),
+    ]
+
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents=prompt
+        model='gemini-2.0-flash-001',
+        contents=messages
     )
+
     prompt_token_count = response.usage_metadata.prompt_token_count
     prompt_response_count = response.usage_metadata.candidates_token_count
+    if len(sys.argv) > 2:
+        argument = sys.argv[2]
+        if argument == "--verbose":
+            print(f"User prompt: {prompt}")
+            print(f"Prompt tokens: {prompt_token_count}")
+            print(f"Response tokens: {prompt_response_count}")
 
     print(response.text)
-    print(f"Prompt tokens: {prompt_token_count}")
-    print(f"Response tokens: {prompt_response_count}")
+
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         prompt = sys.argv[1]
-        main()
+        main(prompt)
     else:
         sys.exit("Please provide a prompt. Example: python3 main.py \"where all the spice melange at?\"")
